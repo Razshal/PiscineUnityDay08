7fa549d43ab07da9e425a4f77cdea6b2dfcfbccc
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 20:43:26 +0200

    ex03 - LAST ANIMATION BUG FOUND ++

--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    private int frameCount;
 
     // Player ui references
-    public Slider lifeSlider;    public Slider XpSlider;    public Text lifeText;    public Text xpText;    public Text lvlText;    public GameObject enemyInfosPanel;    public Slider enemyLifeSlider;    public Text enemyName;    public Text enemyLevel;    public GameObject enemyHover;    new void Start()    {        base.Start();        camera = Camera.main;        lifeSlider.maxValue = life;        XpSlider.maxValue = requieredXp;        displayName = "Maya";        experience = 0;    }    private void OnTriggerStay(Collider other)
+    public Slider lifeSlider;    public Slider XpSlider;    public Text lifeText;    public Text xpText;    public Text lvlText;    public GameObject enemyInfosPanel;    public Slider enemyLifeSlider;    public Text enemyName;    public Text enemyLevel;    public GameObject enemyHover;    public GameObject statsUI;    public int skillPoints;    public ParticleSystem levelup;    new void Start()    {        base.Start();        camera = Camera.main;        displayName = "Maya";        experience = 0;    }    private void OnTriggerStay(Collider other)
     {
         if (state != State.ATTACKING            && !enemyTarget            && !other.isTrigger            && other.gameObject.CompareTag("Enemy")
             && other.gameObject.GetComponent<CharacterScript>().state != State.DEAD            && !prioritaryWaypoint)        {
             enemyTarget = other.gameObject;
         }
-    }    public void ReceiveExperience(int newXp)    {        experience += newXp;        if (experience > requieredXp)        {            experience -= requieredXp;            requieredXp += 150;            level += 1;            life = maxLife;        }        XpSlider.value = experience;        lvlText.text = "LVL " + level;    }    private void UpdateUi()    {        CharacterScript enemyToDisplay = null;
+    }    public void ReceiveExperience(int newXp)    {        experience += newXp;        if (experience >= requieredXp)        {            experience -= requieredXp;            requieredXp += 150;            level += 1;            life = maxLife;            skillPoints += 5;            levelup.time = 0;            levelup.Play();        }    }    private void UpdateUi()    {        CharacterScript enemyToDisplay = null;
 
         // Determine wich value needs to be displayed
         if (enemyTarget)            enemyToDisplay = enemyTarget.GetComponent<CharacterScript>();        else if (enemyHover)            enemyToDisplay = enemyHover.GetComponent<CharacterScript>();                // Then if enemy, display his infos        if (enemyToDisplay)        {
             enemyInfosPanel.SetActive(true);
             enemyLifeSlider.maxValue = enemyToDisplay.maxLife;            enemyLifeSlider.value = enemyToDisplay.life;            enemyName.text = enemyToDisplay.displayName;            enemyLevel.text = "LVL " + enemyToDisplay.level;
-        }        else            enemyInfosPanel.SetActive(false);        // Update the player ones        lifeSlider.value = life;        lifeText.text = life + "/" + lifeSlider.maxValue;    }    new void Update()    {        base.Update();
+        }        else            enemyInfosPanel.SetActive(false);        // Update the player ones        XpSlider.value = experience;		XpSlider.maxValue = requieredXp;        xpText.text = experience + "/" + requieredXp;        lvlText.text = "LVL " + level;        lifeSlider.value = life;        lifeText.text = life + "/" + lifeSlider.maxValue;        lifeSlider.maxValue = maxLife;    }    new void Update()    {        base.Update();
 
         // Updating UI
         UpdateUi();
@@ -26,4 +26,4 @@
             navMeshAgent.SetDestination(clickHit.point);
             prioritaryWaypoint = true;
             enemyTarget = null;
-        }    }}
+        }        if (Input.GetKeyDown(KeyCode.C))            statsUI.SetActive(!statsUI.activeSelf);    }}
