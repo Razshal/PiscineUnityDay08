f5edc98b867878600dbc665e2653d258f56e10b9
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 14:37:11 +0200

    ex02 - improving target system

--- d08/Assets/Scripts/CharacterScript.cs
+++ d08/Assets/Scripts/CharacterScript.cs
@@ -1,7 +1,12 @@
 ﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class CharacterScript : MonoBehaviour
-{    protected Animator animator;    protected NavMeshAgent navMeshAgent;    public int maxLife = 3;    public int life;    private bool isPlayer;    public bool isInContact;    public GameObject enemyTarget;    private bool deadTrigger = false;    private bool deathCoroutine = false;    public bool isAlive = true;
+{    protected Animator animator;    protected NavMeshAgent navMeshAgent;
+    public GameObject enemyTarget;    public int maxLife = 3;    public int life;    public bool isInContact;    public bool isAlive = true;
     // Allows the player to get out of fight
-    protected bool prioritaryWaypoint = false;    public enum State    {        RUN,        ATTACKING,        IDLE,        DEAD    }    public State state;    protected void Start()    {        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();        animator = gameObject.GetComponent<Animator>();        isPlayer = gameObject.CompareTag("Player");        life = maxLife;    }    protected void UpdateAnimation()    {        string attackVar = isPlayer ? "Attack" : "ZombieAttack";        switch (state)        {            case State.DEAD:                animator.SetBool(attackVar, false);                animator.SetBool("Run", false);                if (!deadTrigger)                {
+    protected bool prioritaryWaypoint = false;
+
+    private bool isPlayer;
+    private bool deadTrigger = false;
+    private bool deathCoroutine = false;    public enum State    {        RUN,        ATTACKING,        IDLE,        DEAD    }    public State state;    protected void Start()    {        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();        animator = gameObject.GetComponent<Animator>();        isPlayer = gameObject.CompareTag("Player");        life = maxLife;    }    protected void UpdateAnimation()    {        string attackVar = isPlayer ? "Attack" : "ZombieAttack";        switch (state)        {            case State.DEAD:                animator.SetBool(attackVar, false);                animator.SetBool("Run", false);                if (!deadTrigger)                {
                     animator.SetTrigger("Dead");                    deadTrigger = true;                }                break;            case State.RUN:                animator.SetBool(attackVar, false);                animator.SetBool("Run", true);                break;            case State.ATTACKING:                animator.SetBool("Run", false);                animator.SetBool(attackVar, true);                break;            default:                animator.SetBool("Run", false);                animator.SetBool(attackVar, false);                break;        }    }    public void ReceiveDamages()    {        life -= 1;        if (life <= 0)            state = State.DEAD;    }    private IEnumerator DeadDisapearing()    {        deathCoroutine = true;        Destroy(gameObject.GetComponent<CharacterController>());        Destroy(gameObject.GetComponent<NavMeshAgent>());        yield return new WaitForSeconds(4);        Destroy(gameObject, 2f);        while (gameObject)        {
             transform.Translate(Vector3.down * 0.005f);
             yield return new WaitForFixedUpdate();
@@ -13,5 +18,6 @@
 
             // Sets states for animations and attack
             if (navMeshAgent.hasPath && !isInContact)                state = State.RUN;            else if (enemyTarget && isInContact && enemyTarget.GetComponent<CharacterScript>().isAlive)            {                transform.LookAt(enemyTarget.transform.position);                state = State.ATTACKING;                prioritaryWaypoint = false;            }            else            {
-                state = State.IDLE;                prioritaryWaypoint = false;            }        }        else if (!deathCoroutine)            StartCoroutine("DeadDisapearing");        
+                state = State.IDLE;                prioritaryWaypoint = false;                if (isPlayer)                    enemyTarget = null;            }        }        else if (!deathCoroutine)            StartCoroutine("DeadDisapearing");
+
         UpdateAnimation();    }}
--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    public bool attackFrame = false;    private int lastAttack = 0;    private int frameCount;    new void Start()    {        base.Start();        camera = Camera.main;    }    private void OnTriggerStay(Collider other)
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    private int lastAttack = 0;    private int frameCount;    // Allows the animation to communicate his attack frame    public bool attackFrame = false;    new void Start()    {        base.Start();        camera = Camera.main;    }    private void OnTriggerStay(Collider other)
     {
         if (state != State.ATTACKING            && !enemyTarget            && !other.isTrigger            && other.gameObject.CompareTag("Enemy")
             && other.gameObject.GetComponent<CharacterScript>().isAlive            && !prioritaryWaypoint)        {
@@ -7,9 +7,7 @@
     }    public void AttackEnnemyForAnimation()    {
         if (state == State.ATTACKING && enemyTarget && attackFrame && lastAttack + 5 < frameCount)
         {            lastAttack = frameCount;            enemyTarget.GetComponent<EnemyScript>().ReceiveDamages();
-            attackFrame = false;            if (!enemyTarget.GetComponent<EnemyScript>().isAlive)            {
-                enemyTarget = null;
-            }
+            attackFrame = false;
         }
     }    new void Update()    {        base.Update();
         AttackEnnemyForAnimation();        frameCount = Time.frameCount;        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {
