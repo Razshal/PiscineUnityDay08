5024851876ca7fad883c461503e32bb190aacb28
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 16:42:25 +0200

    ex03 - ui wip

--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    private int lastAttack = 0;    private int frameCount;    // Allows the animation to communicate his attack frame    public bool attackFrame = false;    new void Start()    {        base.Start();        camera = Camera.main;    }    private void OnTriggerStay(Collider other)
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    private int lastAttack = 0;    private int frameCount;
+    // Allows the animation to communicate his attack frame
+    public bool attackFrame = false;    public Slider lifeSlider;    public Slider XpSlider;    new void Start()    {        base.Start();        camera = Camera.main;        lifeSlider.maxValue = life;        XpSlider.maxValue = requieredXp;    }    private void OnTriggerStay(Collider other)
     {
         if (state != State.ATTACKING            && !enemyTarget            && !other.isTrigger            && other.gameObject.CompareTag("Enemy")
             && other.gameObject.GetComponent<CharacterScript>().state != State.DEAD            && !prioritaryWaypoint)        {
@@ -10,5 +12,18 @@
             attackFrame = false;
         }
     }    public void ReceiveExperience(int newXp)    {        experience += newXp;        if (experience > requieredXp)        {            experience -= requieredXp;            level += 1;        }    }    new void Update()    {        base.Update();
-        AttackEnnemyForAnimation();        frameCount = Time.frameCount;        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {
-            // Set player click movement            if (!clickHit.collider.gameObject.CompareTag("Enemy"))            {                navMeshAgent.SetDestination(clickHit.point);                prioritaryWaypoint = true;                enemyTarget = null;            }        }    }}
+        // Updating UI
+        lifeSlider.value = life;        XpSlider.value = life;        // Will attack if animation communicated the right frame
+        AttackEnnemyForAnimation();
+        // Check if the right attack frame is passed
+        frameCount = Time.frameCount;
+
+        // Sets player click movement instructions
+        if (Input.GetMouseButtonDown(0)
+            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit)
+            && !clickHit.collider.gameObject.CompareTag("Enemy"))
+        {
+            navMeshAgent.SetDestination(clickHit.point);
+            prioritaryWaypoint = true;
+            enemyTarget = null;
+        }    }}
