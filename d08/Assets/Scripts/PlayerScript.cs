a810fde70cf9165d84cc8742647c90cd2abaf435
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 19:03:53 +0200

    ex03 - LAST ANIMATION BUG FOUND

--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    private int frameCount;
 
     // Player ui references
-    public Slider lifeSlider;    public Slider XpSlider;    public Text lifeText;    public Text xpText;    public Text lvlText;    public GameObject enemyInfosPanel;    public Slider enemyLifeSlider;    public Text enemyName;    public Text enemyLevel;    public GameObject enemyHover;    new void Start()    {        base.Start();        camera = Camera.main;        lifeSlider.maxValue = life;        XpSlider.maxValue = requieredXp;        displayName = "Maya";    }    private void OnTriggerStay(Collider other)
+    public Slider lifeSlider;    public Slider XpSlider;    public Text lifeText;    public Text xpText;    public Text lvlText;    public GameObject enemyInfosPanel;    public Slider enemyLifeSlider;    public Text enemyName;    public Text enemyLevel;    public GameObject enemyHover;    new void Start()    {        base.Start();        camera = Camera.main;        lifeSlider.maxValue = life;        XpSlider.maxValue = requieredXp;        displayName = "Maya";        experience = 0;    }    private void OnTriggerStay(Collider other)
     {
         if (state != State.ATTACKING            && !enemyTarget            && !other.isTrigger            && other.gameObject.CompareTag("Enemy")
             && other.gameObject.GetComponent<CharacterScript>().state != State.DEAD            && !prioritaryWaypoint)        {
             enemyTarget = other.gameObject;
         }
-    }    public void ReceiveExperience(int newXp)    {        experience += newXp;        if (experience > requieredXp)        {            experience -= requieredXp;            level += 1;        }        XpSlider.value = experience;        lvlText.text = "LVL " + level;    }    private void UpdateUi()    {        CharacterScript enemyToDisplay = null;
+    }    public void ReceiveExperience(int newXp)    {        experience += newXp;        if (experience > requieredXp)        {            experience -= requieredXp;            requieredXp += 150;            level += 1;            life = maxLife;        }        XpSlider.value = experience;        lvlText.text = "LVL " + level;    }    private void UpdateUi()    {        CharacterScript enemyToDisplay = null;
 
         // Determine wich value needs to be displayed
         if (enemyTarget)            enemyToDisplay = enemyTarget.GetComponent<CharacterScript>();        else if (enemyHover)            enemyToDisplay = enemyHover.GetComponent<CharacterScript>();                // Then if enemy, display his infos        if (enemyToDisplay)        {
