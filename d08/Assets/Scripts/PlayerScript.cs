96b9d00cb4de67b20cc3d4d9f873dc651a854b49
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 11:41:54 +0200

    ex02 - animations debuggued

--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -6,7 +6,14 @@
         if (state == State.ATTACKING && enemyTarget && attackFrame)
         {
             enemyTarget.GetComponent<EnemyScript>().ReceiveDamages();
-            attackFrame = false;
+            attackFrame = false;            if (enemyTarget.GetComponent<EnemyScript>().life < 0)            {
+                state = State.IDLE;
+                enemyTarget = null;
+            }
         }
     }    new void Update()    {        base.Update();
-        AttackEnnemyForAnimation();        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {            // Sets enemy target for mother script            if (clickHit.collider && clickHit.collider.gameObject.CompareTag("Enemy"))                enemyTarget = clickHit.collider.gameObject;            // Or set player click movement            else            {                navMeshAgent.SetDestination(clickHit.point);                enemyTarget = null;            }        }    }}
+        AttackEnnemyForAnimation();        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {
+            // Sets enemy target for mother script
+            if (clickHit.collider && clickHit.collider.gameObject.CompareTag("Enemy"))                enemyTarget = clickHit.collider.gameObject;
+            // Or set player click movement
+            else            {                navMeshAgent.SetDestination(clickHit.point);                enemyTarget = null;            }        }    }}
