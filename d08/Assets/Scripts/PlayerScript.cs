d8bc09eea8eac4ee38468a2f0b4317aa67638b05
Razshal <fontenille.mathieu@gmail.com>
2019-05-31 12:13:27 +0200

    ex02 - animations and damage sync are ok

--- d08/Assets/Scripts/PlayerScript.cs
+++ d08/Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    public bool attackFrame = false;    new void Start()    {        base.Start();        camera = Camera.main;    }    private void OnTriggerEnter(Collider other)
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.AI;public class PlayerScript : CharacterScript{    private RaycastHit clickHit;    new private Camera camera;    public bool attackFrame = false;    private int lastAttack = 0;    private int frameCount;    new void Start()    {        base.Start();        camera = Camera.main;    }    private void OnTriggerStay(Collider other)
     {
-        if (!other.isTrigger && other.gameObject.CompareTag("Enemy"))
+        if (!other.isTrigger && other.gameObject.CompareTag("Enemy") && other.gameObject.GetComponent<CharacterScript>().isAlive)
             enemyTarget = other.gameObject;
     }    public void AttackEnnemyForAnimation()    {
-        if (state == State.ATTACKING && enemyTarget && attackFrame)
-        {
-            enemyTarget.GetComponent<EnemyScript>().ReceiveDamages();
-            attackFrame = false;            if (enemyTarget.GetComponent<EnemyScript>().life < 0)            {
-                state = State.IDLE;
-                enemyTarget = null;
+        if (state == State.ATTACKING && enemyTarget && attackFrame && lastAttack + 5 < frameCount)
+        {            lastAttack = frameCount;            enemyTarget.GetComponent<EnemyScript>().ReceiveDamages();
+            attackFrame = false;            if (enemyTarget.GetComponent<EnemyScript>().life <= 0)            {
+                state = State.IDLE;                enemyTarget = null;
             }
         }
     }    new void Update()    {        base.Update();
-        AttackEnnemyForAnimation();        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {
+        AttackEnnemyForAnimation();        frameCount = Time.frameCount;        if (Input.GetMouseButtonDown(0)            && Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out clickHit))        {
             // Sets enemy target for mother script
             if (clickHit.collider && clickHit.collider.gameObject.CompareTag("Enemy"))                enemyTarget = clickHit.collider.gameObject;
             // Or set player click movement
